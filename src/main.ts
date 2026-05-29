e03b8e922b8dc9a37913d3970f231761fe0e28ad
lasha <maisuradze.llasha@gmail.com>
2026-05-29 19:42:07 +0400

    try Analytics

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { App } from './app/app';
 import { routes } from './app/app-routing-module';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 bootstrapApplication(App, {
   providers: [
